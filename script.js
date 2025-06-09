ef4c6e54684ce9c337a33f01a00e016aa3e63f96
prajapatirupesh69 <prajapatirupesh669@gmail.com>
2025-06-09 22:29:47 +0930

    Disabled Function script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+/*
 function showSection(id) {
   const sections = document.querySelectorAll('.content-section');
   sections.forEach(sec => sec.classList.remove('active'));
   document.getElementById(id).classList.add('active');
 }
+*/
