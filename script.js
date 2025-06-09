34a40446652289b54a83349a2937b70d3f9b8cb6
prajapatirupesh69 <prajapatirupesh669@gmail.com>
2025-06-09 22:14:15 +0930

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showSection(id) {
+  const sections = document.querySelectorAll('.content-section');
+  sections.forEach(sec => sec.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
+}
